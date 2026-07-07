70a676bd3773aa4b0f82fb2bf79b185baa21a844
Saty-aNarayan <satyanarayan.jaganathan2001@gmail.com>
2026-07-07 20:43:45 +0530

    writing the javecode

---
+++ data-pipeline/src/main/java/com.worldcup/WorldCupPipelineApplication.java
@@ -0,0 +1,16 @@
+public class WorldCupPipelineApplication {
+
+    public static void main(String[] args) {
+
+        TournamentDownloader downloader = new TournamentDownloader();
+
+        String fileContents = downloader.downloadTournamentData();
+
+        System.out.println(fileContents);
+
+        // TODO:
+        // Pass data to parser
+        // Write JSON
+    }
+
+}
